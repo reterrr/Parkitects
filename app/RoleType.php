5b2bcc0de2041b5d1dad0723e8500e06896854b7
yhwach <sharonovnikola1@gmail.com>
2024-04-21 22:45:09 +0200

    RoleType.php

---
+++ app/RoleType.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use App\Attributes\AttributableEnum;
+use App\Attributes\RolePriority;
+
+enum RoleType: string
+{
+    use AttributableEnum;
+
+    #[RolePriority(1)]
+    case SUPER_ADMIN = 'super-admin';
+
+    #[RolePriority(2)]
+    case ADMIN = 'admin';
+
+    #[RolePriority(3)]
+    case USER = 'user';
+}
